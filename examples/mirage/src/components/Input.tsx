dbb26242d4379953a9c3cc5c0e49b33421f6287a
Peter Perlepes <p.perlepes@gmail.com>
2021-09-03 18:08:35 +0300

    Fix email error notification :bug:

--- examples/mirage/src/components/Input.tsx
+++ examples/mirage/src/components/Input.tsx
@@ -3,10 +3,11 @@ import styles from "./Input.module.css";
 
 export const Input = React.forwardRef<
   HTMLInputElement,
-  { helperText?: string }
->(({ helperText, ...rest }, ref) => {
+  { helperText?: string; errorText?: string }
+>(({ helperText, errorText, ...rest }, ref) => {
   return (
     <>
+      {errorText && <span className={styles.errorText}>{errorText}</span>}
       {helperText && <span className={styles.helperText}>{helperText}</span>}
       <input className={styles.input} ref={ref} {...rest} />
     </>
